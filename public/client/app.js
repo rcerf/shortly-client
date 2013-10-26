30af31b5fc96ad19799f1d8b19bbfb4cfb4a0313
rcerf <rickcerf@gmail.com>
2013-10-25 18:45:53 -0700

    index and create render to page

---
+++ public/client/app.js
@@ -0,0 +1,39 @@
+angular.module('shortlyApp', [])
+.config(function($routeProvider){
+  $routeProvider
+  .when('/', {
+    controller: "LinkControl",
+    templateUrl: '/client/template/link.html'
+  })
+  .when('/shorten', {
+    controller: "ShortenControl",
+    templateUrl: '/client/template/shorten.html'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+})
+.controller('LinkControl',
+  function($scope, $http){
+    $http.get('/links')
+    .success(function(data, status, headers, config){
+      $scope.link = data;
+    })
+    .error(function(data, status){
+
+    });
+  })
+.controller('ShortenControl',
+  function($scope, $http){
+    $scope.link = null;
+    $scope.submit = function(){
+      $http.post('/links', {url: $scope.link})
+          .success(function(data, status, headers, config){
+            console.log("Posted!");
+          }).error(function(data, status){
+            console.error("Problem posting.");
+            console.log($scope.link);
+            console.log(data);
+          });
+    };
+  });
